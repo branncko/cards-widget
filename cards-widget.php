362bd4c7ab815c95532b6104c7d2e5eb04676473
branncko <imprimarte@gmail.com>
2020-08-24 17:31:36 -0300

    NTI

--- cards-widget.php
+++ cards-widget.php
@@ -19,7 +19,7 @@ class Card extends WP_Widget {
     public function __construct()
     {
         $widget_ops = array (
-            'classname' => 'card'
+            'classname' => 'card',
             'description' => 'Exibe o conteúdo em um card'
 
         );
