0a2927ad688916bff9c5449311ffdba339517d96
branncko <imprimarte@gmail.com>
2020-08-23 21:45:27 -0300

    Construindo o form

--- cards-widget.php
+++ cards-widget.php
@@ -10,17 +10,21 @@ Author URI: https://brancosousa.com.br/imgfree
 
 //registrar o widget
 add_action('widget_init', function() {
-    register_widget('Cards');
+    register_widget('Card');
 });
 
-class Cards extends WP_Widget {
+class Card extends WP_Widget {
     
     //Método construtor da classe
     public function __construct()
     {
-        $widget_ops = array {
-            'classname'= 'cards'
-        
+        $widget_ops = array (
+            'classname' => 'card'
+            'description' => 'Exibe o conteúdo em um card'
+
+        );
+        parent::__construct('card', 'Card', $widget_ops);
+                    
     }
 
     //Método que mostra a saída no$intance Front End
@@ -32,8 +36,34 @@ class Cards extends WP_Widget {
     // Método para cadastro no painel
     public function form($instance)
     {
-
+            $title = '';
+            if (!empty($instance['title']))
+            {
+                $title = $instance['title'];
+            }
+           
+            $description = '';
+            if (!empty($instance['description']))
+            {
+                $description = $instance['description'];
+            }
+           
+            $link_url = '';
+            if (!empty($instance['link_url']))
+            {
+                $link_url = $instance['link_url'];
+            }
+            
+            ?>
+            <p>
+                <label for="<?php echo $this ->get_field_name('title'); ?>">
+                    <?php echo 'Título:' ?>
+                </label>
+            </p>
+            <?php
     }
+
+    
     
     // Método que atualiza o Widget
     public function update($new_instance, $old_instance)
